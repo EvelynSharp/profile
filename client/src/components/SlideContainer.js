e0edd292cf1cab8269ddabd9946b2f6fcefc6c9c
Evelyn Sharp <yihongshi90@gmail.com>
2017-08-04 18:41:02 -0600

    resize event

--- client/src/components/SlideContainer.js
+++ client/src/components/SlideContainer.js
@@ -1,10 +1,27 @@
 import React from 'react';
 
 class SlideContainer extends React.Component {
+  state={ height: 500}
+
+  componentDidMount = () => {
+    this.findHeight();
+    if( typeof window !== 'undefined' )
+      window.addEventListener('resize', this.findHeight, true)
+  }
+
+  findHeight = () => {
+    let height = window.innerHeight;
+    if ( height < 500) {
+      this.setState( {height: 500} )
+    } else {
+      this.setState( { height } )
+    }
+  }
 
   render() {
+    let { height } = this.state;
     return(
-      <div style={{ height: '100px', margin: '100px'}}>
+      <div style={{ height: height + 'px', border: '1px solid red'}}>
         { this.props.children }
       </div>
     )
